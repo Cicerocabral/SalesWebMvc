a98c798cc385a16011f9f12818ec39bcd6a0db00
Cicero Cabral <cicerocabral.ti@gmail.com>
2021-01-20 17:04:47 -0300

    commit ate onde parei

---
+++ SalesWebMvc/Services/Exceptions/DbCourrencyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbCourrencyException : ApplicationException
+    {
+        public DbCourrencyException (string message) : base(message)
+        {
+
+        }
+    }
+}
